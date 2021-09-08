a76e9a195c0053df4b83b631468021b540eeae30
Jiefei Wang <wangjfmh@gmail.com>
2021-09-07 22:49:24 -0400

    Finished the word

--- app.js
+++ app.js
@@ -1,31 +1,77 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const { models: { User }} = require('./db');
+const { models: { User, Note }} = require('./db');
 const path = require('path');
-const jwt = require('jsonwebtoken')
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    // to verify the token is right or wrong by User.byToken function -
+    // - jwt.verify(token, process.env.JWT) to get the payload -
+    // - if it's right, the function will return the user
+    // so User.byToken will get the user is the token is right.
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error)
+  }
+}
+
 app.post('/api/auth', async(req, res, next)=> {
   try {
-    res.send({ token: await User.authenticate(req.body)});
+    // to get the user which its password match the database password
+    const user = await User.authenticate(req.body);
+    if (!user) res.sendStatus(404);
+
+    // to get the token by jwt.sign in user.generateToken function
+    // the token contains user's id information
+    const token = await user.generateToken();
+    res.send(token)
   }
   catch(ex){
     next(ex);
   }
 });
 
-app.get('/api/auth', async (req, res, next)=> {
+app.get('/api/auth', requireToken, async (req, res, next)=> {
   try {
-    // res.send(await User.byToken(req.headers.authorization));
-    const token = await jwt.verify(req.headers.authorization, process.env.JWT)
-    res.send(await User.byToken(token));
+    if (req.user) {
+      res.send(req.user);
+    } else {
+      res.sendStatus(404);
+    }
+
   }
   catch(ex){
     next(ex);
   }
 });
+
+app.get('/api/users/:id/notes', requireToken, async (req, res, next) => {
+  try {
+    // if my token id is 1, and my api request is
+    // /api/users/2/notes
+    // this will previously pass, when it should fail
+
+    // here, req.params.id is not a number
+    // we could use != or Number() to compare the two id
+    if (req.user.id != req.params.id) {
+      throw new Error('Unauthorized');
+    }
+    const userNotes = await Note.findAll({
+      where: {
+        userId: req.params.id
+      }
+    });
+    res.send(userNotes);
+  } catch (error) {
+    next(error)
+  }
+})
 
 app.use((err, req, res, next)=> {
   console.log(err);
--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-
 const config = {
   logging: false
 };
@@ -10,27 +9,53 @@ const config = {
 if(process.env.LOGGING){
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
 
-const User = conn.define('user', {
+const User = db.define('user', {
   username: STRING,
   password: STRING,
 }, {
   hooks: {
     async beforeCreate(user) {
-      const password = user.password;
-      const hashed = await bcrypt.hash(password, 5);
-      user.password = hashed;
+      user.password = await bcrypt.hash(user.password, 5);
     }
   }
 });
 
+const Note = db.define('note', {
+  text: STRING
+})
+
+Note.belongsTo(User);
+User.hasMany(Note);
+
+// to get the token by jwt.sign function
+User.prototype.generateToken = async function() {
+  try {
+    const token = await jwt.sign({ id: this.id }, process.env.JWT);
+    return {token};
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// to get the user if the token is right by jwt.verify function
 User.byToken = async(token)=> {
   try {
-    const user = await User.findByPk(token.id);
-    if(user){
+    // const user = await User.findByPk(token.id);
+    // if(user){
+    //   return user;
+    // }
+    const payload = await jwt.verify(token, process.env.JWT);
+    if (payload) {
+      // find user by payload which contains the user id
+      const user = await User.findByPk(payload.id);
       return user;
     }
+
     const error = Error('bad credentials');
     error.status = 401;
     throw error;
@@ -42,34 +67,51 @@ User.byToken = async(token)=> {
   }
 };
 
+// to get the user if the password is match to the database by bcrypt.compare
 User.authenticate = async({ username, password })=> {
   const user = await User.findOne({
     where: {
       username,
     }
   });
-  const hashed = user.password;
-  const authenticatePass = await bcrypt.compare(password, hashed)
-  if(authenticatePass){
-    // return user.id;
-    return jwt.sign({ id: user.id }, process.env.JWT)
+  const match = await bcrypt.compare(password, user.password)
+  if(match){
+    return user
   }
   const error = Error('bad credentials');
   error.status = 401;
   throw error;
 };
 
+// another way to write the hook function
+// before create the data, change the plain password as hash by bcrypt.hash()
+// User.addHook('beforeCreate', async (user) => {
+//   if (user.changed('password')) {
+//     user.password = await bcrypt.hash(user.password, 5)
+//   }
+// })
+
 const syncAndSeed = async()=> {
-  await conn.sync({ force: true });
+  await db.sync({ force: true });
   const credentials = [
     { username: 'lucy', password: 'lucy_pw'},
     { username: 'moe', password: 'moe_pw'},
     { username: 'larry', password: 'larry_pw'}
   ];
-
   const [lucy, moe, larry] = await Promise.all(
     credentials.map( credential =>  User.create(credential))
   );
+  const notes = [
+    { text: "hello world" },
+    { text: "reminder to buy groceries" },
+    { text: "reminder to do laundry" },
+  ];
+  const [note1, note2, note3] = await Promise.all(
+    notes.map(note => Note.create(note))
+  );
+  await lucy.setNotes(note1);
+  await moe.setNotes([note2, note3]);
+
   return {
     users: {
       lucy,
@@ -82,6 +124,7 @@ const syncAndSeed = async()=> {
 module.exports = {
   syncAndSeed,
   models: {
-    User
+    User,
+    Note
   }
 };
